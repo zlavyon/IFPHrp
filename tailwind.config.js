70011432ad3775bc8d2aed7bb293f8a076c5c816
rvillaroman <raphael_villaroman@dlsu.edu.ph>
2025-11-18 22:36:50 +0800

    TailwindCSS update, node packages for flexibility

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.html",
+    "./**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
